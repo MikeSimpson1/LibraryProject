8fb82184c607a4ec6004543e9ea32ef5601e588e
MikeSimpson1 <Petworker6@gmail.com>
2022-01-13 14:20:19 -0500

    Added BookInfo page with skeleton setup

--- Data/BookService.cs
+++ Data/BookService.cs
@@ -7,7 +7,7 @@ public class BookService
 {
     HttpClient client = new HttpClient();
     private readonly IServiceScopeFactory scopeFactory;
-    public BookService (IServiceScopeFactory scopeFactory)
+    public BookService(IServiceScopeFactory scopeFactory)
     {
         this.scopeFactory = scopeFactory;
     }
@@ -22,11 +22,17 @@ public class BookService
             bookTitle = bookTitle.Replace(" ", "+");
             bookAuthor = bookAuthor.Replace(" ", "+");
 
+            /*  
+             *  TODO:
+             *  This needs to work. First query the database, and then query google.
+             * 
+             */
             List<BookData> fromDb = GetBookDataFromDbAsync(bookTitle, bookAuthor);
             if (fromDb.Count >= 10)
             {
                 return fromDb;
             }
+            //
 
             string QueryString = "https://www.googleapis.com/books/v1/volumes?q=" + bookTitle + "+inauthor:" + bookAuthor;
             var stringTask = await client.GetStringAsync(QueryString);
@@ -35,34 +41,55 @@ public class BookService
             {
                 foreach (var book in bookQuery.items)
                 {
-                    string title = book.volumeInfo.title;
-                    string author = book.volumeInfo.authors[0];
-                    string published = book.volumeInfo.publishedDate;
+                    string title = "";
+                    string author = "";
+                    string published = "";
                     string image = "https://www.ianrankin.net/wp-content/uploads/2018/07/missingbook.png";
+                    string isbn = "";
+                    string pagecount = "0";
+                    string description = "";
+                    try
+                    {
+                        title = book.volumeInfo.title;
+                    }
+                    catch (Exception e) { Console.Write(e.Message); }
+
+                    try
+                    {
+                        author = book.volumeInfo.authors[0];
+                    }
+                    catch (Exception e) { Console.Write(e.Message); }
+
+                    try
+                    {
+                        published = book.volumeInfo.publishedDate;
+                    }
+                    catch (Exception e) { Console.Write(e.Message); }
+                    
                     try
                     {
                         image = book.volumeInfo.imageLinks.thumbnail;
                     }
                     catch (Exception e) { Console.Write(e.Message); }
-                    string isbn = "";
+
                     try
                     {
                         isbn = book.volumeInfo.industryIdentifiers[0].identifier;
                     }
                     catch (Exception e) { Console.Write(e.Message); }
-                    string pagecount = "0";
+
                     try
                     {
                         pagecount = book.volumeInfo.pageCount;
                     }
                     catch (Exception e) { Console.Write(e.Message); }
 
-                    string description = "";
                     try
                     {
                         description = book.volumeInfo.description;
                     }
                     catch (Exception e) { Console.Write(e.Message); }
+
                     BookData b = new(title, author, published, image, isbn, pagecount, description);
                     books.Add(b);
                     if (!db.Books.Contains(b))
@@ -83,6 +110,15 @@ public class BookService
             var db = scope.ServiceProvider.GetRequiredService<BookDataDbContext>();
             List<BookData> l = db.Books.AsQueryable().Where(b => b.Title.Contains(title)).ToList<BookData>(); //|| b.Author.Contains(author)
             return l;
+        }
+    }
+
+    public BookData GetBookByISBN(string isbn)
+    {
+        using (var scope = scopeFactory.CreateScope())
+        {
+            var db = scope.ServiceProvider.GetRequiredService<BookDataDbContext>();
+            return db.Books.Find(isbn);
         }
     }
 }

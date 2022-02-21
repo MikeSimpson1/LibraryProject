16b9a3d3677fe955ee51b7a3bbdb571c01c3e9b1
MikeSimpson1 <Petworker6@gmail.com>
2022-02-20 22:57:14 -0500

    Updated accountService

--- Data/AccountService.cs
+++ Data/AccountService.cs
@@ -1,7 +1,13 @@
-﻿namespace LibraryApplication.Data
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LibraryApplication.Data
 {
     public class AccountService
     {
+        public AccountService(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+
+        }
         public Boolean RegisterUser(AppUser appUser)
         {
             return true;
